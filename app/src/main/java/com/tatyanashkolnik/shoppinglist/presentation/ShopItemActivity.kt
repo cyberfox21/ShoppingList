e1d578e543ae00add984b3b4e9d4e2f49fbb6e28
Tatyana Shkolnik <shkolniktatyana21@gmail.com>
2021-08-07 14:08:59 +0300

    Layout and ShopItemActivity created.

---
+++ app/src/main/java/com/tatyanashkolnik/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.tatyanashkolnik.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tatyanashkolnik.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
