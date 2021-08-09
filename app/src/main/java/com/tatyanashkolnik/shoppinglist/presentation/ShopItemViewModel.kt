ce038f93f59604a700f3a8508785913aecb9d0d3
Tatyana Shkolnik <shkolniktatyana21@gmail.com>
2021-08-09 15:15:26 +0300

    Finish ShopItemViewModel.

--- app/src/main/java/com/tatyanashkolnik/shoppinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/tatyanashkolnik/shoppinglist/presentation/ShopItemViewModel.kt
@@ -1,7 +1,8 @@
 package com.tatyanashkolnik.shoppinglist.presentation
 
-import android.text.TextUtils.isDigitsOnly
 import androidx.core.text.isDigitsOnly
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.tatyanashkolnik.shoppinglist.data.ShopListRepositoryImpl
 import com.tatyanashkolnik.shoppinglist.domain.AddShopItemUseCase
@@ -17,37 +18,63 @@ class ShopItemViewModel : ViewModel() {
     private val addShopItemUseCase = AddShopItemUseCase(repository)
     private val getShopItemUseCase = GetShopItemUseCase(repository)
 
-    fun getShopItem(shopItemId: Int): ShopItem? {
-        return getShopItemUseCase.getShopItem(shopItemId)
+    private val _shouldCloseScreen = MutableLiveData<Unit>() // тип Unit обозначает то, что нам
+                                           // не важно какой объект мы устанавливаем в LiveData,
+                                           // важно лишь то, что будет оповещение(из-за изменения)
+    val shouldCloseScreen : LiveData<Unit>
+    get() = _shouldCloseScreen
+
+    // true -> показываем юзеру ошибку | false -> всё ок
+    // private _ чтобы устанавливать значения от сюда, из ViewModel
+    private val _errorInputName = MutableLiveData<Boolean>()
+    // публичная чтобы получить в Activity
+    val errorInputName: LiveData<Boolean> // у типа LiveData нельзя устанавливать значение
+        get() = _errorInputName// переопр getter чтобы возвращал значение приватной переменной
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem : LiveData<ShopItem>
+        get() = _shopItem
+
+    fun getShopItem(shopItemId: Int) {
+        val item = getShopItemUseCase.getShopItem(shopItemId)
+        _shopItem.value = item!!
     }
 
-    fun addShopItem(inputName: String?, inputCount: String?){
+    fun addShopItem(inputName: String?, inputCount: String?) {
         val name = parseName(inputName)
         val count = parseCount(inputCount)
         val fieldsValid = validateInput(name, count)
-        if(fieldsValid){
+        if (fieldsValid) {
             val shopItem = ShopItem(name, count, true)
             addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
         }
-
     }
 
-    fun editShopItem(inputName: String?, inputCount: String?){
+    fun editShopItem(inputName: String?, inputCount: String?) {
         val name = parseName(inputName)
         val count = parseCount(inputCount)
         val fieldsValid = validateInput(name, count)
-        if(fieldsValid){
-            val shopItem = ShopItem(name, count, true)
-            editShopItemUseCase.editShopItem(shopItem)
+        if (fieldsValid) {
+            _shopItem.value?.let {
+                val item = it.copy(name = name, count = count) // копируем старый объект,
+                // чтобы сохранить его id и состояние enabled, но изменяем отредактированные поля
+                editShopItemUseCase.editShopItem(item)
+                finishWork()
+            }
         }
     }
 
-    private fun parseName(name: String?): String{
+    private fun parseName(name: String?): String {
         return name?.trim() ?: ""
     }
 
-    private fun parseCount(count: String?) : Int{
-        val result = when{
+    private fun parseCount(count: String?): Int {
+        val result = when {
             count?.isDigitsOnly() == true -> count.toInt() ?: 0
             count?.contains(" ") == true -> count.trim().toInt() ?: 0
             else -> 0
@@ -55,17 +82,29 @@ class ShopItemViewModel : ViewModel() {
         return result
     }
 
-    private fun validateInput(name: String, count: Int): Boolean{
+    private fun validateInput(name: String, count: Int): Boolean {
         var result = true
-        if(name.isBlank()){
-            // TODO(Show error input name)
+        if (name.isBlank()) {
+            _errorInputName.value = true
             result = false
         }
-        if(count <= 0) {
-            // TODO(Show error input count)
+        if (count <= 0) {
+            _errorInputCount.value = true
             result = false
         }
         return result
     }
 
+    fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    fun resetErrorInputCount() {
+        _errorInputCount.value = false
+    }
+
+    private fun finishWork(){
+        _shouldCloseScreen.value = Unit // сам объект, который прилетит в observer,
+                                        // в активити никак использоваться не будет
+    }
 }
